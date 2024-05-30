7923553df43c72553dbf8048303d4dc7d5233436
ASHA LEARNOLOGY <ashalearnology.inst@gmail.com>
2024-05-30 17:48:58 +0530

    first commit

---
+++ app/index.jsx
@@ -0,0 +1,21 @@
+import {View,Text} from "react-native";
+
+export default function App() {
+  const arrobject = [
+    {
+      title: "prabhu",
+      age: 27,
+      roll: 26,
+    }
+  ]
+  const arrobjects = [
+  ];
+
+  return (
+    <View>
+      {arrobject.map(()=>{
+        
+      }) }
+    </View>
+  );
+}
