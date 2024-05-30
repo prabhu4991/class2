8c81d4b271de2f8be7947ac4830548095018f4db
ASHA LEARNOLOGY <ashalearnology.inst@gmail.com>
2024-05-30 19:16:12 +0530

    create

--- app/index.jsx
+++ app/index.jsx
@@ -1,23 +1,73 @@
-import {View,Text} from "react-native";
+import { View, Text } from "react-native";
+import { useState } from "react";
+import { TextInput, } from "react-native-gesture-handler";
 
 export default function App() {
-  const arrobject = [
+
+  const [inputput, setInputValue] = useState("");
+  const [array1, setArray1] = useState([
     {
-      title: "prabhu",
-      age: 27,
-      roll: 26,
-    }
-  ]
+      title: "prabhu 0",
+    },
 
+    {
+      title: "prabhu 1",
 
+    },
 
-  fytdtd
+    {
+      title: "prabhu 2",
+    },
+
+    {
+      title: "prabhu 3",
+    },
+
+    {
+      title: "prabhu 4",
+    },
+
+  ]);
+
+  function chnageArray(){
+    const newArray=[
+      ...array1,
+      {
+        title: inputValue,
+      },
+    ];
+  }
 
   return (
     <View>
-      {arrobject.map(()=>{
-        
-      }) }
+      <TextInput
+        value={inputput}
+        onPressText={setInputValue}
+        placeholder="add data"
+      />
+
+      {array1.map((item, index) => {
+        return (<Text key={index}>
+          {item.title}
+        </Text>)
+      })}
+
+      <pressable 
+      onPress={chnageArray}
+
+      style={{
+        backgroundColor:"red",
+        width:"100",
+        height:"70",
+      borderRadius:20,
+      marginLeft:150,
+          }}
+      >
+<Text>
+  sumbit
+</Text>
+      </pressable>
+
     </View>
   );
 }
